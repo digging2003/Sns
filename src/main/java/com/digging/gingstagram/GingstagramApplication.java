453b16eb17bcfa46d0427cb378d9a4d104a4ccfa
digging <digging@gmail>
2025-03-11 21:05:23 +0900

    [이지원] - Gingstagram 프로젝트 추가

---
+++ src/main/java/com/digging/gingstagram/GingstagramApplication.java
@@ -0,0 +1,13 @@
+package com.digging.gingstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GingstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GingstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digging/gingstagram/GingstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digging.gingstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GingstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
